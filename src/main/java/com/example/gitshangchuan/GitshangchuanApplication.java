d64ca97197763b9e8ad3567b93c2054e0df27c64
qingyu <l983047670@163.com>
2022-05-16 20:33:28 +0800

    Initial commit

---
+++ src/main/java/com/example/gitshangchuan/GitshangchuanApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitshangchuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitshangchuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitshangchuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitshangchuan/GitshangchuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitshangchuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitshangchuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
